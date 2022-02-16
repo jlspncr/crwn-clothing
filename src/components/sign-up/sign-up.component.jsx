fcc8734fc984712781be6b3626a4f894c71bb60a
jlspncr <jlspncer@gmail.com>
2022-02-15 20:05:19 -0500

    Finishing sign in handleSubmit method

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -61,7 +61,7 @@ class SignUp extends React.Component {
           <FormInput 
           type='text' 
           name='displayName' 
-          value={displayName} 
+          value={this.state.displayName} 
           onChange={this.handleChange} 
           label='Display Name' 
           required
@@ -70,7 +70,7 @@ class SignUp extends React.Component {
           <FormInput 
           type='email' 
           name='email' 
-          value={email} 
+          value={this.state.email} 
           onChange={this.handleChange} 
           label='Email' 
           required
@@ -79,7 +79,7 @@ class SignUp extends React.Component {
           <FormInput 
           type='password' 
           name='password' 
-          value={password} 
+          value={this.state.password} 
           onChange={this.handleChange} 
           label='Password' 
           required
@@ -88,7 +88,7 @@ class SignUp extends React.Component {
           <FormInput 
           type='password' 
           name='confirmPassword' 
-          value={confirmPassword} 
+          value={this.state.confirmPassword} 
           onChange={this.handleChange} 
           label='Confirm Password' 
           required
